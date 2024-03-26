fa6431e6683432563b0f14d47f71b77054a5b4e1
mimao-littlemouse <mimao_littlemouse@163.com>
2024-03-26 14:05:01 +0800

    导航栏 已完成

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,11 @@ layui.use(function(){
     'mobile-header-navbar-menu-button': function(){
       let element = document.querySelector("#mobileHeaderNavbarMenuContent")
       layer.open({
-        title: ['更多', 'font-size: 12px;cursor:default;user-select: none;'],
+        title: ['<i class="layui-icon layui-icon-app" style="font-size:20px"></i>', 'color:white;cursor:default;user-select: none;background:linear-gradient(111deg, #c1b2b2, #62cced, #c1b2b2, #c1b2b2, #c1b2b2, transparent);'],
         type: 1,
         offset: 'r',
         anim: 'slideLeft', // 从右往左
-        area: ['70%', '100%'],
+        area: ['85%', '100%'],
         shade: 0.5,
         shadeClose: false,
         move: false,
@@ -24,7 +24,7 @@ layui.use(function(){
           // layero 弹层的最外层元素的 jQuery 对象
           // index 弹层的索引值
           // that 弹层内部原型链中的 this 
-          
+
           // 获取 关闭按钮 元素 并进行图标设置
           for(let child of layero[0].children){
             if(child.classList.contains('layui-layer-setwin')){
@@ -60,20 +60,36 @@ layui.use(function(){
     <div class="col-4 ps-4 d-flex align-items-center user-select-none" >
       <img src="/src/assets/logo.png" alt="" style="widows: 45px;height: 45px;">
       <!-- 桌面端显示 移动端隐藏  -->
-      <span class="d-none d-md-block mixiaomao-text">
+      <span class="d-none d-md-block mixiaomao-text mixiaomao-gradient">
         MiXiaoMao
       </span>
     </div>
 
     <!-- 桌面端 导航栏部分 d-none d-md-block-->
-    <nav class="col-8 d-none d-md-flex align-items-center justify-">
-      
+    <nav class="col-8 d-none d-md-flex align-items-center justify-content-end pe-3">
+      <div class="row w-100 h-100">
+        <!-- 左 区域 -->
+        <div class="col-10 h-100 d-flex justify-content-end">
+          <ul class="m-0 p-0 d-flex h-100 header-navbar-menu">
+            <li class="mx-2 px-3 menu-item text-center h-100 d-flex align-items-center text-white">首页</li>
+            <li class="mx-2 px-3 menu-item text-center h-100 d-flex align-items-center text-white">关于</li>
+          </ul>
+        </div>
+        <!-- 右 区域 -->
+        <div class="col-2 h-100 d-flex justify-content-end align-items-center">
+          <div style="width: 60px;" class="text-center">
+            <a href="https://github.com/mimao-littlemouse/mixiaomao" target="_blank">
+              <i class="layui-icon layui-icon-github" style="font-size: 24px;"></i> 
+            </a>
+          </div>
+        </div>
+      </div>
     </nav>
 
     <!-- 移动端 导航栏部分 d-block d-md-none -->
     <!-- 中部 文字部分 -->
     <div class="col-4 d-block d-md-none d-flex justify-content-center align-items-center user-select-none">
-      <span class="mixiaomao-text">MiXiaoMao</span>
+      <span class="mixiaomao-text mixiaomao-white">MiXiaoMao</span>
     </div>
     <!-- 右侧 展开 按钮 -->
     <div lay-on="mobile-header-navbar-menu-button" class="col-4 d-block d-md-none d-flex align-items-center justify-content-end">
@@ -81,12 +97,22 @@ layui.use(function(){
     </div> 
   </header>
   <!-- 移动端 导航展开部分  d-none -->
-  <nav id="mobileHeaderNavbarMenuContent" class="d-flex align-items-center d-none px-2">
-    <nav>
-      display
-    </nav>
+  <nav id="mobileHeaderNavbarMenuContent" class="d-flex flex-column d-none" style="background: lightgray;">
+    <div class="d-flex" style="height: 50px;">
+      <ul class="m-0 mt-1 p-0 d-flex w-100 h-100 header-navbar-menu text-bg-light">
+        <li class="mx-2 px-3 menu-item text-center h-100 d-flex align-items-center">首页</li>
+        <li class="mx-2 px-3 menu-item text-center h-100 d-flex align-items-center">关于</li>
+      </ul>
+    </div>
+    <div class="d-flex align-items-center" style="height: 50px;background-color: #d2eff9;">
+      <div style="width: 60px;" class="text-center">
+        <a href="https://github.com/mimao-littlemouse/mixiaomao" target="_blank">
+          <i class="layui-icon layui-icon-github" style="font-size: 24px;"></i> 
+        </a>
+      </div>
+    </div>
   </nav>
-  <main style="height: calc(100vh);" class="bg-info">
+  <main style="height: calc(100vh);">
     
   </main>
   <footer style="height: 60px;" class="bg-danger">
@@ -105,9 +131,27 @@ header{
   .mixiaomao-text{
     font-weight: bold;
     font-size: 18px;
-    background: linear-gradient(120deg,lightblue,lightgreen,#3e71af 100%);
+  }
+  .mixiaomao-gradient{
+    background: linear-gradient(90deg,#5f5f5f 20%, #16b777 60%, #f8f8f8 20%);
     -webkit-background-clip: text;
     -webkit-text-fill-color: transparent;
+  }
+  .mixiaomao-white{
+    background: white;
+    opacity: 0.8;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+  }
+}
+.header-navbar-menu{
+  list-style: none;
+  .menu-item:hover{
+    opacity: 0.5;
+    cursor: default;
+  }
+  .menu-item:active{
+    opacity: 1;
   }
 }
 </style>
